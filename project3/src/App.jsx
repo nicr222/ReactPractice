5de7790d39c39753a0e845bfbaadb452e0fc1163
Nicolas Ross <15972995@mstc.edu>
2024-07-02 15:14:36 -0500

    Set up UserInput Component

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -1,8 +1,10 @@
+import UserInput from "./components/UserInput.jsx";
 import Header from "./components/header.jsx";
 
 function App() {
   return (
     <Header />
+    <UserInput />
   )
 }
 
--- project3/src/components/header.jsx
+++ project3/src/components/header.jsx
@@ -6,5 +6,6 @@ export default function Header() {
             <img src={logo} alt='Logo showing a money bag'/>
          <h1>Investment Calculator</h1>
         </header>
+        
     );
 }
